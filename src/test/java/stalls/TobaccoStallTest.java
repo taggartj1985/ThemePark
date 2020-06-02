1b0dd733358fd635cf873d5b9fdfe3e1ebabc5a9
John Taggart <taggartj1985@gmail.com>
2020-06-02 16:45:16 +0100

    people of age are allowed to buy stuff from tobaccostall and if under rejected

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,21 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitor;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitor = new Visitor(18, 12, 100);
+        visitor2 = new Visitor(12, 10, 20);
     }
 
     @Test
@@ -28,4 +33,15 @@ public class TobaccoStallTest {
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
     }
+
+    @Test
+    public void isAllowed(){
+        assertEquals(true, tobaccoStall.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void isNotAllowed(){
+        assertEquals(false, tobaccoStall.isAllowedTo(visitor2));
+    }
+
 }
