cb55d58c625cb9faa918c9dcfd343987b0855e8f
John Taggart <taggartj1985@gmail.com>
2020-06-02 16:37:37 +0100

    test for person entering or being deninded in playground working

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor visitor;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitor = new Visitor(15, 12, 100);
+        visitor2 = new Visitor(16, 12, 100);
     }
 
     @Test
@@ -26,5 +31,15 @@ public class PlaygroundTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void isAllowed(){
+        assertEquals(true, playground.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void isNotAllowed(){
+        assertEquals(false, playground.isAllowedTo(visitor2));
     }
 }
