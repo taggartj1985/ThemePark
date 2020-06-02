df608f61f4aeedf1e21551df7ea035aa631c3391
John Taggart <taggartj1985@gmail.com>
2020-06-02 16:54:05 +0100

    rollercoaster ISecruity done and tests working

--- src/main/java/attractions/RollerCoaster.java
+++ src/main/java/attractions/RollerCoaster.java
@@ -1,8 +1,19 @@
 package attractions;
 
-public class RollerCoaster  extends Attraction {
+import behaviours.ISecurity;
+import people.Visitor;
+
+public class RollerCoaster  extends Attraction implements ISecurity {
 
     public RollerCoaster(String name, int rating) {
         super(name, rating);
+    }
+
+    public boolean isAllowedTo(Visitor visitor){
+        if(visitor.getHeight() > 145 && visitor.getAge() > 12){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,23 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor oldButSmallVisitor;
+    Visitor youngButTallVisitor;
+    Visitor vistorCanRideRoller;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        oldButSmallVisitor = new Visitor(100, 12, 10);
+        youngButTallVisitor = new Visitor(12, 10000, 1000);
+        vistorCanRideRoller = new Visitor(13, 146, 190);
     }
 
     @Test
@@ -27,5 +34,17 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void isAllowedToride(){assertEquals(true, rollerCoaster.isAllowedTo(vistorCanRideRoller));
+    }
+
+    @Test
+    public void isNotAllowedTorideDueToHeight(){assertEquals(false, rollerCoaster.isAllowedTo(oldButSmallVisitor));
+    }
+
+    @Test
+    public void isAllowedTorideDueToAge(){assertEquals(false, rollerCoaster.isAllowedTo(youngButTallVisitor));
     }
 }
